2084fd81fc7521a9de5f5b52a2f84c2cc4dcc2c8
Bouchra ABIDAR <bouchra_abidar@carrefour.com>
2022-04-11 21:40:32 +0200

    update requirements

--- startup_script.sh
+++ startup_script.sh
@@ -2,9 +2,10 @@
 export FLASK_APP=app.py
 export FLASK_ENV=development
 
-python3 -m venv app-env
-source app-env/bin/activate
-pip install --upgrade pip
-pip flask==2.0.3
+python3 -m venv app-env 
+source app-env/bin/activate 
+pip install --upgrade pip 
+pip install flask==2.0.3 
+pip install flask_sqlalchemy
 
 flask run
